9415cb1970a24619db1bbe7b5ac3ec76ecba409f
Benike1 <tothben10@gmail.com>
2021-09-06 22:55:25 +0200

    add todo list to users

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -41,7 +41,7 @@
             <th>Name</th>
             <th>Email</th>
             <th>Roles</th>
-            <th width="280px">Action</th>
+            <th>Action</th>
         </tr>
         @foreach ($data as $key => $user)
             <tr>
